15efa0c86180b03c4bebce4c1cf71dd0a3d1289d
David Dobryakov <davidobryakov@gmail.com>
2021-12-15 01:54:59 +0300

    [fix] Fix auto refreshing

--- djoser_cookie_auth/auth_app/middleware/JWTAutoRefreshCookie.py
+++ djoser_cookie_auth/auth_app/middleware/JWTAutoRefreshCookie.py
@@ -9,33 +9,49 @@ class JWTAutoRefreshCookieMiddleware:
 
     def __call__(self, request):
         refresh_token = request.COOKIES.get('refresh')
-        refresh = RefreshToken(refresh_token)
-
-        data = {'access': str(refresh.access_token)}
-
-        if api_settings.ROTATE_REFRESH_TOKENS:
-
-            refresh.set_jti()
-            refresh.set_exp()
-
-            data['refresh'] = str(refresh)
-
         response = self.get_response(request)
 
-        response.set_cookie(
-            'access',
-            data.get("access"),
-            5,
-            httponly=True,
-            expires=datetime.datetime.now() + datetime.timedelta(minutes=5)
-        )
+        url = request.path
 
-        response.set_cookie(
-            'refresh',
-            data.get("refresh", refresh_token),
-            5,
-            httponly=True,
-            expires=datetime.datetime.now() + datetime.timedelta(days=1)
-        )
+        disabled_urls = ['login', 'logout']
+
+        is_url_disabled = len(
+            list(
+                filter(lambda _url: _url in url, disabled_urls)
+            )
+        ) > 0
+
+        if refresh_token and not is_url_disabled:
+            refresh = RefreshToken(refresh_token)
+
+            data = {'access': str(refresh.access_token)}
+
+            if api_settings.ROTATE_REFRESH_TOKENS:
+
+                refresh.set_jti()
+                refresh.set_exp()
+
+                data['refresh'] = str(refresh)
+
+            request.COOKIES['access'] = data.get('access')
+            request.COOKIES['refresh'] = data.get('refresh', refresh_token)
+
+            response = self.get_response(request) 
+
+            response.set_cookie(
+                'access',
+                data.get("access"),
+                5,
+                httponly=True,
+                expires=datetime.datetime.now() + datetime.timedelta(minutes=5)
+            )
+
+            response.set_cookie(
+                'refresh',
+                data.get("refresh", refresh_token),
+                5,
+                httponly=True,
+                expires=datetime.datetime.now() + datetime.timedelta(days=1)
+            )
 
         return response
